61f60a225c61d7ff551948074c4a7dc2c419c2c1
Cynthia Tong <tsaiyuen@gmail.com>
2023-11-20 22:18:20 -0300

    Create teacher controller

--- src/main/java/teacher/finder/app/controller/TeachersController.java
+++ src/main/java/teacher/finder/app/controller/TeachersController.java
@@ -1,17 +1,25 @@
 package teacher.finder.app.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import teacher.finder.app.teacher.DadosCadastroTeacher;
+import teacher.finder.app.teacher.Teacher;
+import teacher.finder.app.teacher.TeacherRepository;
 
 @RestController
 @RequestMapping("/teachers")
 public class TeachersController {
 
+    @Autowired
+    private TeacherRepository repository;
+
     @PostMapping
-    public void cadastrar(@RequestBody String json){
-        System.out.println(json);
+    public void cadastrar(@RequestBody DadosCadastroTeacher dados){
+        repository.save(new Teacher(dados));
     }
 
 }
+
